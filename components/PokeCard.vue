0016401c7d9334c792d622cf1960f5a134ce3559
Jorge Caballero <joalbes@gmail.com>
2023-04-10 13:30:00 -0500

    203/04/10 1:29pm

---
+++ components/PokeCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="card-nombre"
+                class="card bg-red-600 rounded-md border-4 border-solid border-[#ffffff] shadow-lg shadow-black hover:cursor-pointer">
+                <div class="p-4">
+                    <img class="w-[250px] h-[250px] rounded-md"
+                        src=""
+                        alt="">
+                </div>
+                <div>
+                    <p class="font-bold italic text-center text-2xl text-[#ffffff] p-2">bulbasaur</p>
+                </div>
+            </div>
+</template>
+<script></script>
+<style></style>
--- pages/pokeDEx.vue
+++ pages/pokeDEx.vue
@@ -2,18 +2,9 @@
     <div class="bg-teal-200 p-5">
         <p class="bg-[#ffffff] rounded-[20px] pb-4 text-lg md:text-7xl font-bold text-center mt-[20px] "> PokeDEx</p>
         <input class="my-4 rounded-md border-2 border-solid border-[#C2C2C2] focus:border-[#694DF9] outline-none
-            active:border-[#E2DCFF] hover:border-[#694DF9] p-1" type="text" placeholder="Buscar Pokemon">
+                active:border-[#E2DCFF] hover:border-[#694DF9] p-1" type="text" placeholder="Buscar Pokemon">
         <div id="container-pokes" class="flex flex-wrap md:gap-[40px] gap-[20px] justify-start">
-            <div id="card-bulbasaur"
-                class="card bg-red-600 rounded-md border-4 border-solid border-[#ffffff] shadow-lg shadow-black hover:cursor-pointer">
-                <div class="p-4">
-                    <img class="w-[250px] h-[250px] rounded-md" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/1.png" alt="sus">
-                </div>
-                <div>
-                    <p class="font-bold italic text-center text-2xl text-[#ffffff] p-2">bulbasaur</p>
-                </div>
-
-            </div>
+            
         </div>
     </div>
 </template>
